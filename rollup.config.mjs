27adf813926ef91e7f906b24268c0959f199ca25
Tsunfire <underrated@gmail.com>
2023-01-27 00:43:39 -0800

    ✨ add css bundling

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -24,11 +26,15 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+
+    external: [/\.css$/],
   },
 ];
--- src/components/SwapWidget/SwapWidget.tsx
+++ src/components/SwapWidget/SwapWidget.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./SwapWidget.css";
 
 export interface SwapWidgetProps {
   label: string;
 }
 
 const SwapWidget = ({ label }: SwapWidgetProps) => {
-  return <></>;
+  return <button>{label}</button>;
 };
 
 export default SwapWidget;
